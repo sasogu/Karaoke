758eff4f6832203ad0bfeb4435acbdb8a6b91f82
Blasco <sasogu@asdf.es>
2026-02-20 12:45:31 +0100

    tamaño texto pre

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 
-const APP_CACHE_VERSION = "1.0.10";
+const APP_CACHE_VERSION = "1.0.11";
 const CACHE_PREFIX = "karaoke-pwa";
 const STATIC_CACHE = `${CACHE_PREFIX}-static-${APP_CACHE_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${APP_CACHE_VERSION}`;
