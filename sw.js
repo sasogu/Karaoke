709c14a58744712d6084d45cd19a051c8719c86c
Blasco <sasogu@asdf.es>
2026-02-26 10:22:32 +0100

    demo eliminada

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 
-const APP_CACHE_VERSION = "1.1.2";
+const APP_CACHE_VERSION = "1.1.3";
 const CACHE_PREFIX = "karaoke-pwa";
 const STATIC_CACHE = `${CACHE_PREFIX}-static-${APP_CACHE_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${APP_CACHE_VERSION}`;
